5b71d6d8757fb3195170f97237bdcdd2231f0377
master1521 <master1521@yandex.ru>
2021-11-11 21:22:32 +0400

    Праввки

--- Scraping/game.py
+++ Scraping/game.py
@@ -26,6 +26,7 @@ class Client:
 	def load_page(self, url):
 		""" Принимает url и возвращает html страницу в виде текста """
 		req = self.session.get(url)
+		req.status_code()
 		return req.text  # req.status_code
 
 	def pars_page(self, src):
@@ -136,3 +137,4 @@ class Client:
 if __name__ == '__main__':
 	person = Client()
 	person.run('data', 3)
+
