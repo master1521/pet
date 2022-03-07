f93c7d4e8e2f1298a4fae61a99e20b94f6dbedd4
master1521 <master1521@yandex.ru>
2022-03-07 10:22:34 +0400

    Добавил файл с содзанием базы и sql запросы

--- book/book.sql
+++ book/book.sql
@@ -4,6 +4,7 @@
 --Список из покупатели которые купиль больше всего книг?
 --Посчитайсе минимальный, средний, максимальный платеж и сумму всех платежей для каждого покупателя
 
+--EXPLAIN ANALYZE 
 SELECT
 --	c.id_customer 
 	c.first_name
@@ -19,24 +20,6 @@ LEFT JOIN customer c ON p.id_customer = c.id_customer
 GROUP BY c.id_customer 
 ORDER BY count_payment DESC
 LIMIT 10
-
--- или так
-SELECT
-	c.id_customer
-	,c.first_name 
-	,c.last_name 
-	,min(b.price) OVER w AS min_payment
-	,avg(b.price) OVER w AS avg_payment
-	,max(b.price) OVER w AS max_payment
-	,sum(b.price) OVER w sum_all_payment
-	,count(p.id_payment) OVER w AS count_payment
---	,count(p.id_payment) OVER (PARTITION BY c.id_customer ORDER BY DESC) AS count_payment
-FROM payment p
-LEFT JOIN book b ON p.id_book = b.id_book
-LEFT JOIN customer c ON p.id_customer = c.id_customer
-WINDOW w AS (PARTITION BY c.id_customer)
-ORDER BY count_payment DESC, id_customer ASC
---ORDER BY count_payment DESC
 
 
 --Список из 10 самых продоваемых книг
@@ -134,35 +117,5 @@ LEFT JOIN book b ON b.id_book = p.id_book
 )
 SELECT * FROM t
 WHERE ROW_NUMBER <= 2 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
